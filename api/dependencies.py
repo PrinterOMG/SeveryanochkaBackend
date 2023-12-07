ecf4b5080260af48c7a67362db3cd88314f3a276
PrinterOMG <PrinterOMG@ya.ru>
2023-12-07 21:24:58 +0300

    Fix tokenUrl in OAuth2 scheme

--- api/dependencies.py
+++ api/dependencies.py
@@ -11,7 +11,7 @@ from utils.unit_of_work import UnitOfWorkBase, UnitOfWork
 
 UOWDep = Annotated[UnitOfWorkBase, Depends(UnitOfWork)]
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/auth/login')
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/api/auth/login')
 
 
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], uow: UOWDep):
