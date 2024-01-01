35ab3d35d351873673409c82dceb9db4dc70fa6d
PrinterOMG <PrinterOMG@ya.ru>
2024-01-01 20:37:05 +0300

    Add password validation for register and reset Now it must be between 8 and 64 chars long

--- api/schemas/auth.py
+++ api/schemas/auth.py
@@ -16,7 +16,10 @@ class Token(BaseModel):
 
 class RegisterRequest(BaseModel):
     phone_key: str = Field(title='Phone key', description='Must be valid verified phone key')
-    password: str
+    password: str = Field(
+        title='Password',
+        description='Must be between 8 and 64 characters long', min_length=8, max_length=64
+    )
 
 
 class RegisterResult(BaseModel):
@@ -26,4 +29,7 @@ class RegisterResult(BaseModel):
 
 class ResetPasswordRequest(BaseModel):
     phone_key: str = Field(title='Phone key', description='Must be valid verified phone key')
-    password: str
+    password: str = Field(
+        title='Password',
+        description='Must be between 8 and 64 characters long', min_length=8, max_length=64
+    )
