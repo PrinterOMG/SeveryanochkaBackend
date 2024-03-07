3be75b032512bb56ab3ff7633d9e495470eb506e
printeromg <PrinterOMG@ya.ru>
2024-03-07 20:00:21 +0300

    Fix brands tag

--- api/routers/brands.py
+++ api/routers/brands.py
@@ -7,7 +7,7 @@ from api.schemas.brand import BrandRead, BrandCreate, BrandUpdate
 from api.schemas.other import ErrorMessage
 from database.models import Brand
 
-router = APIRouter(prefix='/brands', tags=['BrandS'])
+router = APIRouter(prefix='/brands', tags=['Brands'])
 
 
 @router.get('')
